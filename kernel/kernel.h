cd93258b2a8359f05eaa6269dddeee9bef4f86fa
brianStremel <brianstremel@gmail.com>
2019-06-01 00:29:38 -0300

    Se movieron funciones de kernel.c a kernel.h

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -32,7 +32,67 @@ typedef struct {
 #include <readline/readline.h>
 #include "../biblioteca/biblioteca_sockets.h"
 
+/*TAMANIODEARCHIVO
+ * Devuelve el tamaño de un archivo.
+ */
+int32_t tamanioDeArchivo(FILE* f) {
+	int32_t previo = ftell(f);
+	fseek(f, 0, SEEK_END);
+	int32_t tamanio = ftell(f);
+	fseek(f, previo, SEEK_SET);
+	return tamanio;
+}
 
+
+
+/* JOURNAL
+ * Le envia a memoría el comando para realizar el JOURNAL.
+ */
+void journal() {
+	printf("---select\n");
+	int16_t servidor = conectarseA(IP_LOCAL, PUERTO_ESCUCHA_MEM);
+	enviarMensaje(JOURNAL, servidor);
+	close(servidor);
+
+}
+
+
+/*	PARSEARLQL
+ * Recibe un archivo y un array de punteros, aloca cada fila del txt
+ * en una posición del array y retorna la cantidad de elementos que tiene
+ * el archivo.
+ */
+
+//FUNCIONA PERO SI AL FINAL DEL TXT HAY UN "ENTER" DEVUELVE BASURA
+int16_t parsearLQL(FILE* f, char** buffer) {
+	int32_t tamanioArchivo = tamanioDeArchivo(f);
+	int16_t i = 0;
+	while(!feof(f)){
+		char* auxiliar = malloc(tamanioArchivo);
+		buffer[i] = malloc(30);
+
+		fgets(auxiliar, tamanioArchivo, f);
+		if(strlen(auxiliar) > 0){
+			strcpy(buffer[i], auxiliar);
+			i++;
+		}
+
+		free(auxiliar);
+	}
+
+	return i;
+}
+
+
+
+
+/*PARSEAR POR ESPACIO
+ *
+ */
+char** parsear(char** cosa, char* cadena) {
+	cosa = string_split(cadena, " ");
+	return cosa;
+}
 
 
 
