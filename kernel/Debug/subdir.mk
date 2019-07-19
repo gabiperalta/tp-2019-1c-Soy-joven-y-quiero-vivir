44838fd856cdbd2d74a2d8e492e1f4e0c19d2d7f
gabiperalta <gabiperalta16398@gmail.com>
2019-07-19 01:41:34 -0300

    Intento de kernel-memoria-fs

--- kernel/Debug/subdir.mk
+++ kernel/Debug/subdir.mk
@@ -4,12 +4,15 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../funcionesKernel.c \
 ../kernel.c 
 
 OBJS += \
+./funcionesKernel.o \
 ./kernel.o 
 
 C_DEPS += \
+./funcionesKernel.d \
 ./kernel.d 
 
 
