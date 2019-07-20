db594c0b945e20cebb2fa77a286bd1ef23ab6d79
fedebanca <fedebanca97@gmail.com>
2019-07-19 21:16:42 -0300

    agregamos el deploy

---
+++ deploy.sh
@@ -0,0 +1,77 @@
+cd
+
+mkdir workspace
+
+cd
+
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+
+cd so-commons-library/
+
+sudo make install
+
+cd
+
+curl -L faq.utnso.com/script-ntp.sh | bash
+
+git clone https://github.com/sisoputnfrba/parsi.git
+
+cd parsi/
+
+sudo make install
+
+cd
+
+
+
+cd /home/utnso/Proyectos/tp-2018-1c-PC-citos/libreria/Debug
+
+sudo make
+
+
+
+sudo cp liblibreria.so /usr/lib/
+
+
+
+cd 
+
+
+
+cd /home/utnso/Proyectos/tp-2018-1c-PC-citos/Coordinador/Debug/
+
+make clean
+
+make all
+
+cd
+
+
+
+cd /home/utnso/Proyectos/tp-2018-1c-PC-citos/Instancia/Debug/
+
+make clean
+
+make all
+
+cd
+
+
+
+cd /home/utnso/Proyectos/tp-2018-1c-PC-citos/ESI/Debug/
+
+make clean
+
+make all
+
+cd
+
+
+
+cd /home/utnso/Proyectos/tp-2018-1c-PC-citos/Planificador/Debug/
+
+make clean
+
+make all
+
+cd
